589b574e81363dcf743e3095153fd66d3e158028
mahekunnisa <mahek.unnisa@elysiumlabs.in>
2023-10-15 00:48:09 +0530

    Added Ga tag

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import { Navbar, Summarizer, ImageGen, Footer } from './components'
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import { useEffect } from 'react';
+import ReactGA from 'react-ga';
 
 const App = () => {
+  useEffect(() => {
+    ReactGA.initialize(import.meta.env.VITE_GTAG_ID);
+    ReactGA.pageview(window.location.pathname);
+  }, []);
+
   return (
     <main>
       <div className='main'>
